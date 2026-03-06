d49066d3cab88752f420b78079a4ef72df8e731e
KZ7777 <kaoutarzaid@hotmail.com>
2026-03-06 09:41:13 +0100

    week 5 blog geadd

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -23,5 +23,14 @@ export const blogs = [
         description: ' In week 4 hebben we verder gewerkt met Docker, ditmaal met focus op het gebruik van volumes. Daarnaast hebben we de bijhorende documentatie verder uitgewerkt en een meeting gehouden waarin we een demo gaven van Docker met volumes. Vervolgens hebben we de volledige opstelling opnieuw opgebouwd met PostgreSQL versie 16 als voorbereiding op de toekomstige upgrade binnen het bedrijf. Momenteel werkt het bedrijf nog met versie 14, maar we hebben getest of alles correct functioneert in versie 16. Daarbij hebben we getest of de werking identiek bleef en of alles correct functioneerde binnen deze omgeving. Tot slot kregen we uitleg over de volgende stap: het werken met de echte database van het bedrijf, eerst in een lokale omgeving, voordat dit verder wordt uitgerold.',
         image: ''
         
+    },
+    {
+        id:4,
+        date: '02/03/2026 - 06/03/2026',
+        title: 'Week 5: Grote database restore en docker',
+        category: ['PostgreSQL', 'Linux', 'Terminal','Docker','Moving-Forward'] ,
+        description: 'Deze week hebben we gewerkt aan de grote database. Eerst hebben we de database lokaal gerestored om het proces goed te begrijpen. Daarna hebben we dezelfde opzet uitgevoerd met Docker containers. Zo konden we zien hoe je een databaseomgeving op een meer gestructureerde manier kan beheren. Op woensdag zijn we ook naar het Moving Forward-event geweest. Het was een leuk en aangenaam event waar we de kans kregen om met verschillende mensen te praten en te socializen. Het was interessant om nieuwe contacten te leggen en andere mensen uit de sector te ontmoeten.',
+        image: './assets/moving_forward_event.png'
+        
     }
 ];
